6c4ad6788ad6df2173803d5ad729ebefee5d45f4
rmzahid <rmzahid@juniv.edu>
2018-07-20 14:43:06 +0600

    Add files via upload

---
+++ Exam System CSE.mm
@@ -0,0 +1,149 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Exam System CSE" ID="ID_1706802146" CREATED="1264319532085" MODIFIED="1272524369149"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Official correspondance" POSITION="right" ID="ID_1956387586" CREATED="1264319590719" MODIFIED="1299048883262">
+<icon BUILTIN="full-2"/>
+<node TEXT="Starting of semester" ID="ID_396680244" CREATED="1264319631001" MODIFIED="1299048915856">
+<node TEXT="Semester start,date" ID="ID_1503566231" CREATED="1264319936718" MODIFIED="1264320856458"/>
+<node TEXT="Exam start date tentative" ID="ID_1953188147" CREATED="1264320857112" MODIFIED="1264320869494"/>
+<node TEXT="subject to be offered for a session" ID="ID_1770993953" CREATED="1264319958621" MODIFIED="1264319977280"/>
+</node>
+<node TEXT="teacher of subject from Chairman" ID="ID_1381455811" CREATED="1264319645609" MODIFIED="1299048917966">
+<node TEXT="Chairman login" ID="ID_244701831" CREATED="1264319840574" MODIFIED="1264319846182"/>
+<node TEXT="Select session, year, semester" ID="ID_653740257" CREATED="1264319846733" MODIFIED="1264319862035"/>
+<node TEXT="enter teacher code for specific subject" ID="ID_1370235731" CREATED="1264319863496" MODIFIED="1264319884124"/>
+</node>
+<node TEXT="student enroll at dept Exam section" ID="ID_1747462409" CREATED="1264319658912" MODIFIED="1299048919911">
+<icon BUILTIN="full-3"/>
+<node TEXT="student enter roll" ID="ID_209495118" CREATED="1264319763372" MODIFIED="1264319773739">
+<node TEXT="if first year, all info" ID="ID_1445559995" CREATED="1264321236851" MODIFIED="1264321251167"/>
+<node TEXT="later only minor edit such as hall" ID="ID_1164743227" CREATED="1264321251762" MODIFIED="1264321273893"/>
+</node>
+<node TEXT="subject form appears" ID="ID_982403214" CREATED="1264319774463" MODIFIED="1264319794527">
+<node TEXT="if has cross in previous semester select that also" ID="ID_1381372738" CREATED="1264321293855" MODIFIED="1264321330025"/>
+<node TEXT="should fillup seperate form??" ID="ID_687778387" CREATED="1264321364945" MODIFIED="1264321384720"/>
+</node>
+<node TEXT="fillup subject to be taken" ID="ID_1837166310" CREATED="1264319795085" MODIFIED="1264319809832"/>
+<node TEXT="print the form" ID="ID_1420509603" CREATED="1264319814868" MODIFIED="1264319822260"/>
+<node TEXT="chairman sign and ok status" ID="ID_650014615" CREATED="1264321192430" MODIFIED="1264321221468"/>
+</node>
+<node TEXT="syllabus of a session" ID="ID_405450274" CREATED="1298707507761" MODIFIED="1299048921598">
+<node TEXT="Session" ID="ID_1028512787" CREATED="1298707536718" MODIFIED="1298707542430"/>
+<node TEXT="year, semester" ID="ID_1563036980" CREATED="1298707543311" MODIFIED="1298707588901"/>
+<node TEXT="course content" ID="ID_1747968913" CREATED="1298707555188" MODIFIED="1298707564286">
+<node TEXT="code" ID="ID_627952217" CREATED="1298707620242" MODIFIED="1298707625153"/>
+<node TEXT="title" ID="ID_1449988644" CREATED="1298707625735" MODIFIED="1298707628607"/>
+<node TEXT="content" ID="ID_188959092" CREATED="1298707629330" MODIFIED="1298707632612"/>
+<node TEXT="book list" ID="ID_1927489072" CREATED="1298707633302" MODIFIED="1298707638685"/>
+</node>
+</node>
+</node>
+<node TEXT="result processing" POSITION="left" ID="ID_1909202394" CREATED="1264322505034" MODIFIED="1411536707445">
+<icon BUILTIN="full-1"/>
+<node TEXT="enter tutorial marks" ID="ID_229071144" CREATED="1420278622875" MODIFIED="1420278637345"/>
+<node TEXT="enter 1st examiner marks" ID="ID_1412382598" CREATED="1264322636148" MODIFIED="1264322665549"/>
+<node TEXT="enter 2nd examiner marks" ID="ID_1315977828" CREATED="1264322666289" MODIFIED="1264322677306"/>
+<node TEXT="prepare comparison sheet" ID="ID_868982867" CREATED="1264322677953" MODIFIED="1264322697261"/>
+<node TEXT="choose third examiner" ID="ID_1821750740" CREATED="1264322697791" MODIFIED="1264322711095">
+<node TEXT="letter to exam section" ID="ID_1972419251" CREATED="1264322714514" MODIFIED="1264322726401"/>
+<node TEXT="list of roll for 3rd examination" ID="ID_297318259" CREATED="1264322728717" MODIFIED="1264322753031"/>
+<node TEXT="enter 3rd examiner marks" ID="ID_1739831587" CREATED="1293007460187" MODIFIED="1293007479041"/>
+</node>
+<node TEXT="prepare tabulation sheet" ID="ID_457004487" CREATED="1264322759802" MODIFIED="1264322786496"/>
+<node TEXT="prepare result sheet" ID="ID_1345861394" CREATED="1264322793844" MODIFIED="1264322802610"/>
+<node TEXT="For ease of chairman exam" ID="ID_1255370178" CREATED="1420278599429" MODIFIED="1420278749746">
+<node TEXT="Prepare form for who when submit marks" ID="ID_1664483941" CREATED="1264322541995" MODIFIED="1264322632035"/>
+</node>
+</node>
+<node TEXT="pre exam preparation" POSITION="right" ID="ID_1901910958" CREATED="1264324445038" MODIFIED="1299048876996">
+<node TEXT="Declare exam schedule" ID="ID_1076271317" CREATED="1264320876910" MODIFIED="1264324201128">
+<icon BUILTIN="full-3"/>
+<node TEXT="tentative theory schedule" ID="ID_913739878" CREATED="1264320893537" MODIFIED="1264320911205">
+<node TEXT="letter to exam section on exam schedule" ID="ID_764941980" CREATED="1264321061920" MODIFIED="1264321076832"/>
+<node TEXT="notice to all hal" ID="ID_1744823421" CREATED="1264321106269" MODIFIED="1264321119918"/>
+<node TEXT="drag and drop with calender option" ID="ID_1988611920" CREATED="1264322034750" MODIFIED="1264322055175"/>
+<node TEXT="confilct detection with other exam schedule" ID="ID_1096698703" CREATED="1264322211920" MODIFIED="1264322238105"/>
+</node>
+<node TEXT="tentative lab and viva schedule" ID="ID_584541678" CREATED="1264320912136" MODIFIED="1264320941035">
+<node TEXT="letter to exam section" ID="ID_173018421" CREATED="1264321078875" MODIFIED="1264321092223"/>
+<node TEXT="notice to all hall" ID="ID_622683733" CREATED="1264321123192" MODIFIED="1264322023459"/>
+</node>
+<node TEXT="change in exam schedule unexpectly" ID="ID_844151464" CREATED="1264321015864" MODIFIED="1264321038905">
+<node TEXT="letter to exam section" ID="ID_384154479" CREATED="1264321039878" MODIFIED="1264321056909"/>
+<node TEXT="notice to all hall" ID="ID_354979487" CREATED="1264321143942" MODIFIED="1264321151772"/>
+</node>
+</node>
+<node TEXT="selection of first examiner and second examiner" ID="ID_956180780" CREATED="1264320073768" MODIFIED="1264324210832">
+<icon BUILTIN="full-3"/>
+<node TEXT="class teacher 1st examiner" ID="ID_866826752" CREATED="1264320741041" MODIFIED="1264320755447"/>
+<node TEXT="select other from list of teacher" ID="ID_533430537" CREATED="1264320773746" MODIFIED="1264320792470"/>
+<node TEXT="tentative moderation date" ID="ID_977426113" CREATED="1264320807596" MODIFIED="1264320827283"/>
+<node TEXT="a letter to exam section " ID="ID_1553447563" CREATED="1264320984466" MODIFIED="1264321000338"/>
+<node TEXT="A form stating who submit question and when" ID="ID_1218131683" CREATED="1264321472777" MODIFIED="1264321537196"/>
+</node>
+<node TEXT="moderation" ID="ID_1893775100" CREATED="1264321580933" MODIFIED="1264321590200">
+<node TEXT="select moderation date" ID="ID_432550162" CREATED="1264321681357" MODIFIED="1264321695173"/>
+<node TEXT="letter to external" ID="ID_1600015086" CREATED="1264321608934" MODIFIED="1264321624544"/>
+<node TEXT="prepare question paper" ID="ID_284909654" CREATED="1264321705279" MODIFIED="1264324235467">
+<icon BUILTIN="full-4"/>
+</node>
+</node>
+<node TEXT="prepare exam invegilation schedule" ID="ID_351636196" CREATED="1264321772606" MODIFIED="1264324217357">
+<icon BUILTIN="full-3"/>
+<node TEXT="select exam schedule date" ID="ID_1038722880" CREATED="1264321802596" MODIFIED="1264321827020"/>
+<node TEXT="auto select 1st examiner as chief" ID="ID_409643896" CREATED="1264321827978" MODIFIED="1264321850813"/>
+<node TEXT="select two or three examiner" ID="ID_16389056" CREATED="1264321851940" MODIFIED="1264321877347"/>
+<node TEXT="if possible according to seniority, fill up form" ID="ID_1945916626" CREATED="1264321917571" MODIFIED="1264321953050"/>
+<node TEXT="change according to desire of teacher, drag &amp; drop" ID="ID_1047377801" CREATED="1264321958208" MODIFIED="1264322007102"/>
+</node>
+</node>
+</node>
+</map>
